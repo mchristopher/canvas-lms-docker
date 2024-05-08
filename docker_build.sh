615d8b886770b70d5e811fd8a301eca831a1904c
Mike Christopher <mike.christopher@consensys.net>
2024-05-08 16:01:08 -0700

    reorg dockerfiles

--- docker_build.sh
+++ docker_build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 #docker build . -t 'mchristopher/canvas-lms-docker:latest'
 docker buildx build --platform linux/amd64,linux/arm64 -t mchristopher/canvas-lms-docker:latest .
+
+docker buildx build --platform linux/amd64,linux/arm64 -t mchristopher/canvas-lms-docker:latest-core --push -f Dockerfile.core . 
+docker buildx build --platform linux/amd64,linux/arm64 -t mchristopher/canvas-lms-docker:latest --push -f Dockerfile.app . 
