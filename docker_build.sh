99f3524fd4f58f27a2ea829aec785150ce5cff90
Mike Christopher <mchristopher@users.noreply.github.com>
2025-04-08 11:44:21 -0700

    clean up build action

--- docker_build.sh
+++ docker_build.sh
@@ -3,4 +3,5 @@
 docker buildx build --platform linux/amd64,linux/arm64 -t mchristopher/canvas-lms-docker:latest .
 
 docker buildx build --platform linux/amd64,linux/arm64 -t mchristopher/canvas-lms-docker:latest-core --push -f Dockerfile.core . 
+#docker build -t mchristopher/canvas-lms-docker:latest-core -f Dockerfile.core . 
 docker buildx build --platform linux/amd64,linux/arm64 -t mchristopher/canvas-lms-docker:latest --push -f Dockerfile.app . 
